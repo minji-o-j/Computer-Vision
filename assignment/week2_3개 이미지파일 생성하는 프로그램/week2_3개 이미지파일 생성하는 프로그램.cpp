c9b284a8bb1dc8d4978d38b84e33c2b21d0bdf3d
minji-o-j <jminji98@gmail.com>
2020-08-19 14:10:42 +0900

    week2_3개 이미지파일 생성하는 프로그램

---
+++ assignment/week2_3개 이미지파일 생성하는 프로그램/week2_3개 이미지파일 생성하는 프로그램.cpp
@@ -0,0 +1,60 @@
+#pragma warning(disable:4996)
+#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+void main()
+{
+	BITMAPFILEHEADER hf; // BMP 파일헤더 14Bytes
+	BITMAPINFOHEADER hInfo; // BMP 인포헤더 40Bytes
+	RGBQUAD hRGB[256]; // 팔레트 (256 * 4Bytes)
+	FILE *fp;
+	fp = fopen("lenna.bmp", "rb");
+	if (fp == NULL) return;
+	fread(&hf, sizeof(BITMAPFILEHEADER), 1, fp);
+	fread(&hInfo, sizeof(BITMAPINFOHEADER), 1, fp);
+	fread(hRGB, sizeof(RGBQUAD), 256, fp);
+	int ImgSize = hInfo.biWidth * hInfo.biHeight;
+	BYTE * Image = (BYTE *)malloc(ImgSize);
+	BYTE * Output1 = (BYTE *)malloc(ImgSize);
+	BYTE * Output2 = (BYTE *)malloc(ImgSize);
+	BYTE * Output3 = (BYTE *)malloc(ImgSize);
+	fread(Image, sizeof(BYTE), ImgSize, fp);
+	fclose(fp);
+	//-----------------------------------------------------------------
+	/* 영상처리 */
+
+	for (int i = 0; i < ImgSize; i++)
+	{
+		Output1[i] = Image[i];
+		Output2[i] = Image[i] + 50;
+		Output3[i] = 255 - Image[i];
+	
+	}
+
+	fp = fopen("output1.bmp", "wb");
+	fwrite(&hf, sizeof(BYTE), sizeof(BITMAPFILEHEADER), fp);
+	fwrite(&hInfo, sizeof(BYTE), sizeof(BITMAPINFOHEADER), fp);
+	fwrite(hRGB, sizeof(RGBQUAD), 256, fp);
+	fwrite(Output1, sizeof(BYTE), ImgSize, fp);
+	fclose(fp);
+
+	fp = fopen("output2.bmp", "wb");
+	fwrite(&hf, sizeof(BYTE), sizeof(BITMAPFILEHEADER), fp);
+	fwrite(&hInfo, sizeof(BYTE), sizeof(BITMAPINFOHEADER), fp);
+	fwrite(hRGB, sizeof(RGBQUAD), 256, fp);
+	fwrite(Output2, sizeof(BYTE), ImgSize, fp);
+	fclose(fp);
+
+	fp = fopen("output3.bmp", "wb");
+	fwrite(&hf, sizeof(BYTE), sizeof(BITMAPFILEHEADER), fp);
+	fwrite(&hInfo, sizeof(BYTE), sizeof(BITMAPINFOHEADER), fp);
+	fwrite(hRGB, sizeof(RGBQUAD), 256, fp);
+	fwrite(Output3, sizeof(BYTE), ImgSize, fp);
+	fclose(fp);
+
+
+	free(Image);
+	free(Output1);
+	free(Output2);
+	free(Output3);
+}
